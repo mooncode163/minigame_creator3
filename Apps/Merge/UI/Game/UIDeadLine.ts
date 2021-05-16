2e4493ce51fae26288356aaf42c0220a4225aa15
mooncode163 <chyfemail163@163.com>
2021-05-16 20:14:18 +0800

    ui

--- Apps/Merge/UI/Game/UIDeadLine.ts
+++ Apps/Merge/UI/Game/UIDeadLine.ts
@@ -1,84 +1,62 @@
 
-import { _decorator, Component, Node, Prefab } from 'cc'; 
+import { _decorator, Component, Node, Prefab, Collider2D, Contact2DType, IPhysics2DContact } from 'cc';
+import { Debug } from '../../../../Common/Debug';
 import { UIView } from '../../../../Common/UIKit/ViewController/UIView';
 import { GameData } from '../../Data/GameData';
 const { ccclass, property, type } = _decorator;
 
 @ccclass('UIDeadLine')
 export class UIDeadLine extends UIView {
-    t=0;
-    isGameFail=false;
+    t = 0;
+    isGameFail = false;
 
-    onLoad () {
+    onLoad() {
         super.onLoad();
         this.node.name = GameData.NameDeadLine;
         this.t = 0;
         this.isGameFail = false;
+
+        // 还需要body勾选回调接口 cocos 的碰撞区检测必须同时有刚体 和unity不一样
+        let collider = this.node.getComponent(Collider2D);
+        if (collider) {
+            collider.on(Contact2DType.BEGIN_CONTACT, this.onBeginContact, this);
+            collider.on(Contact2DType.END_CONTACT, this.onEndContact, this);
+            collider.on(Contact2DType.PRE_SOLVE, this.onPreSolve, this);
+            collider.on(Contact2DType.POST_SOLVE, this.onPostSolve, this);
+        }
+
     }
-    start () {
+    start() {
         super.start();
-    } 
-  
-    onCollisionEnter(other, self) {
-        // console.log(other, self);
-        this.t = 0; 
-        console.log('UIDeadLine onCollisionEnter enter')
     }
-    onCollisionStay(other, self) {
-       
-    }
-    onCollisionExit(other, self) {
-        console.log('UIDeadLine 现在刚离开')
-    }
-
  
-
-     // 只在两个碰撞体开始接触时被调用一次
-     onBeginContact (contact, selfCollider, otherCollider) {
-        this.t = 0; 
-        
+    // 只在两个碰撞体开始接触时被调用一次
+    onBeginContact(selfCollider: Collider2D, otherCollider: Collider2D, contact: IPhysics2DContact | null) {
+        // will be called once when two colliders begin to contact
+        Debug.Log('UIDeadLine OnCollisionEnter2D on collision enter onBeginContact otherCollider.name=' + otherCollider.node.name + " this.name=" + this.node.name);
+     
     }
 
     // 只在两个碰撞体结束接触时被调用一次
-    onEndContact (contact, selfCollider, otherCollider) {
-        
+    onEndContact(selfCollider: Collider2D, otherCollider: Collider2D, contact: IPhysics2DContact | null) {
+        // will be called once when the contact between two colliders just about to end.
+        // Debug.Log('onEndContact');
     }
 
-    // 每次将要处理碰撞体接触逻辑时被调用 碰撞持续,接触时被调用;
-    onPreSolve (contact, selfCollider, other) {
-
-        console.log('UIDeadLine onPreSolve');
-        // this.t += director.getDeltaTime();
-
-        // if (other.node.name != GameData.main.NameBoardLine)
-        // {
-        //     Debug.Log("OnTriggerStay2D t=" + this.t + " name=" + other.node.name);
-        //     var ui = other.node.getComponent(UIMergeItem);
-        //     if (ui != null)
-        //     {
-        //         if (ui.isNew)
-        //         {
-        //             this.t = 0;
-        //         }
-        //         if (this.t >= 2.0)
-        //         {
-             
-        //             this.t = 0;
-        //             if (!this.isGameFail)
-        //             {
-        //                 this.isGameFail = true;
-        //                 // UIGameMerge.main.OnGameFinish(true);
-        //             }
-        //         }
-        //     }
-
-
-        // }
+    // 每次将要处理碰撞体接触逻辑时被调用
+    onPreSolve(selfCollider: Collider2D, otherCollider: Collider2D, contact: IPhysics2DContact | null) {
+        // will be called every time collider contact should be resolved
+        if (otherCollider.node.name == GameData.NameDeadLine) {
+            Debug.Log("UIDeadLine onPreSolve enter other.name=" + otherCollider.node.name);
+        }
     }
 
-    // 每次处理完碰撞体接触逻辑时被调用 碰撞接触更新完后调用,可以获得冲量信息
-    onPostSolve (contact, selfCollider, otherCollider) {
+    // 每次处理完碰撞体接触逻辑时被调用
+    onPostSolve(selfCollider: Collider2D, otherCollider: Collider2D, contact: IPhysics2DContact | null) {
+        // will be called every time collider contact should be resolved
+        // Debug.Log('onPostSolve');
     }
+
 
 }
 
